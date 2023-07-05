96bea1189b310c1c022fcbbb9d5b629b1debbe7f
Juan Dominguez <juanignaciodom3@gmail.com>
2023-07-05 14:54:01 -0300

    initializing repo

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,70 @@
+<script>
+    import backgroundImage from "$lib/assets/background.jpg";
+</script>
+<section class="background" style="background-image: linear-gradient(
+    to bottom, 
+    rgba(0, 0, 0, 0), 
+    rgba(119, 16, 0, 0.8)), 
+    url('{backgroundImage}');">
+    <nav class="navbar">
+        <a class="link" href="/">El ascenso del depredador</a>
+        <a class="link ml-auto" href="/about">Sobre nosotros</a>
+        <a class="link" href="/play">Jugar</a>
+        <button>Descargar</button>
+    </nav>
+    
+    <slot/>
+</section>
+
+<footer class="footer">
+    Desarrollado para Chaco Futura 2023 - JADEv
+</footer>
+
+<style>
+    :global(html) {
+        margin: 0;
+        padding: 0;
+        background-color: #0f0f0f;
+    }
+
+    :global(body) {
+        margin: 0;
+        font-family: sans-serif;
+    }
+
+    .footer {
+        text-align: center;
+        color: antiquewhite;
+        padding: 1rem 0;
+        background-color: rgba(119, 16, 0, 0.8);
+    }
+
+    .ml-auto {
+        margin-left: auto;
+    }
+
+    .navbar {
+        display: flex;
+        column-gap: 1rem;
+        padding: 1.5rem;
+        background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.7), rgba(0, 255, 0, 0));
+        position: sticky;
+    }
+
+    .link {
+        text-decoration: none;
+        color: antiquewhite;
+        font-weight: bold;
+        font-size: 1.5rem;
+    }
+
+    .background {
+        /* background-image: linear-gradient(
+            to bottom, 
+            rgba(0, 0, 0, 0), 
+            rgba(119, 16, 0, 0.8)), 
+            url('../lib/assets/background.jpg'); */
+        background-size: cover;
+        height: 100dvh;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,65 @@
+<script>
+    import landing01 from "$lib/assets/landing-01.jpg";
+</script>
+
+<div class="container">
+    <section class="section-container">
+        <section class="section-01">
+            <h2 class="game-title">Awesome Game Title</h2>
+            <p class="game-description">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam lacinia
+                auctor justo, vitae tempus lectus facilisis sed. Vestibulum sodales
+                mauris sit amet mauris lacinia, vel lobortis odio pellentesque.
+                Curabitur semper ligula id malesuada tincidunt.
+            </p>
+        </section>
+
+        <section class="section-01">
+            <img class="landing-01 glow" src={landing01} alt="Game" />
+        </section>
+    </section>
+</div>
+
+<style>
+    .container {
+        margin: 0 auto;
+        padding: 1.5rem;
+    }
+
+    .section-container {
+        display: flex;
+        column-gap: 2rem;
+    }
+
+    .section-01 {
+        flex: 1;
+    }
+
+    .landing-01 {
+        object-fit: cover;
+        border-radius: 5px;
+        width: 100%;
+        position: relative;
+    }
+
+    .game-title {
+        font-size: 24px;
+        margin: 20px 0;
+    }
+
+    .game-description {
+        font-size: 18px;
+        line-height: 1.5;
+    }
+
+    .glow::after {
+        content: "";
+        position: absolute;
+        top: -5px;
+        left: -5px;
+        right: -5px;
+        bottom: -5px;
+        border-radius: 5px;
+        box-shadow: 0 0 20px rgba(0, 0, 0, 0.5); /* Adjust the shadow color and size as needed */
+    }
+</style>
